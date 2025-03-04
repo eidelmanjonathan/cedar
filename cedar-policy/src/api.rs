9cf1f0ba01d3099958b5b89e97455a0831699463
Jonathan Eidelman <jeid@amazon.com>
2025-03-04 09:30:51 -0500

    more experimenting

--- cedar-policy/src/api.rs
+++ cedar-policy/src/api.rs
@@ -4550,7 +4550,7 @@ action CreateList in Create appliesTo {
     #[test]
     fn principals() {
         let schema = schema();
-        println!("{:?}", schema);
+        // schema.actions().for_each(|a| println!("{:?}", a));
         // let principals = schema.principals().collect::<HashSet<_>>();
         // assert_eq!(principals.len(), 1);
         // let user: EntityTypeName = "User".parse().unwrap();
